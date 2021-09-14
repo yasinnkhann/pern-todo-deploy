01b032e6b9fe9575ad7fd39c80823f5c053b7ab1
Yasin Khan <yasinkhan500@gmail.com>
2021-09-14 16:39:13 -0700

    reverted changes

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,187 +1,85 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react'
 
-const EditTodo = ({ todo }) => {
-  //editText function
+export default function EditTodo({ todo }) {
 
-  const editText = async id => {
-    try {
-      const body = { description };
+    const [description, setDescription] = useState(todo.description);
 
-      //proxy
+    const updateDescription = async e => {
+        e.preventDefault();
+        const body = { description };
 
-      const res = await fetch(`/todos/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-
-      window.location = "/";
-    } catch (err) {
-      console.error(err.message);
+        try {
+            const res = await fetch(`/todos/${todo.todo_id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(body)
+            });
+            console.log(res);
+            window.location = '/';
+        } catch (err) {
+            console.error(err.message)
+        }
     }
-  };
 
-  const [description, setDescription] = useState(todo.description);
-  return (
-    <Fragment>
-      <button
-        type="button"
-        class="btn btn-warning"
-        data-toggle="modal"
-        data-target={`#id${todo.todo_id}`}
-      >
-        Edit
-      </button>
-      {/* id = "id21"*/}
-      <div
-        class="modal"
-        id={`id${todo.todo_id}`}
-        onClick={() => setDescription(todo.description)}
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Edit Todo</h4>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                onClick={() => setDescription(todo.description)}
-              >
-                &times;
-              </button>
-            </div>
-
-            <div class="modal-body">
-              <input
-                type="text"
-                className="form-control"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-warning"
-                data-dismiss="modal"
-                onClick={() => editText(todo.todo_id)}
-              >
+    return (
+        <Fragment>
+            <button 
+            type="button" 
+            className="btn btn-warning" 
+            data-toggle="modal" 
+            data-target={`#id${todo.todo_id}`}>
                 Edit
-              </button>
-              <button
-                type="button"
-                class="btn btn-danger"
-                data-dismiss="modal"
-                onClick={() => setDescription(todo.description)}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
-};
+            </button>
 
-export default EditTodo;
+            <div 
+            className="modal" 
+            id={`id${todo.todo_id}`}
+            onClick={() => setDescription(todo.description)}>
+            <div className="modal-dialog">
+                <div className="modal-content">
 
+                <div className="modal-header">
+                    <h4 className="modal-title">Edit Todo</h4>
+                    <button 
+                    type="button"
+                    className="close" 
+                    data-dismiss="modal"
+                    onClick={() => setDescription(todo.description)}>
+                        &times;
+                    </button>
+                </div>
 
+                <div className="modal-body">
+                    <input
+                    className="form-control"
+                    type="text" 
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
 
-
-
-
-
-
-
-
-
-
-
-// import React, { Fragment, useState } from 'react'
-
-// export default function EditTodo({ todo }) {
-
-//     const [description, setDescription] = useState(todo.description);
-
-//     const updateDescription = async e => {
-//         e.preventDefault();
-//         const body = { description };
-
-//         try {
-//             const res = await fetch(`/todos/${todo.todo_id}`, {
-//                 method: 'PUT',
-//                 headers: { 'Content-Type': 'application/json'},
-//                 body: JSON.stringify(body)
-//             });
-//             console.log(res);
-//             window.location = '/';
-//         } catch (err) {
-//             console.error(err.message)
-//         }
-//     }
-
-//     return (
-//         <Fragment>
-//             <button 
-//             type="button" 
-//             className="btn btn-warning" 
-//             data-toggle="modal" 
-//             data-target={`#id${todo.todo_id}`}>
-//                 Edit
-//             </button>
-
-//             <div 
-//             className="modal" 
-//             id={`id${todo.todo_id}`}
-//             onClick={() => setDescription(todo.description)}>
-//             <div className="modal-dialog">
-//                 <div className="modal-content">
-
-//                 <div className="modal-header">
-//                     <h4 className="modal-title">Edit Todo</h4>
-//                     <button 
-//                     type="button"
-//                     className="close" 
-//                     data-dismiss="modal"
-//                     onClick={() => setDescription(todo.description)}>
-//                         &times;
-//                     </button>
-//                 </div>
-
-//                 <div className="modal-body">
-//                     <input
-//                     className="form-control"
-//                     type="text" 
-//                     value={description}
-//                     onChange={(e) => setDescription(e.target.value)}
-//                     />
-//                 </div>
-
-//                 <div className="modal-footer">
-//                     <button 
-//                     type="button"
-//                     className="btn btn-warning" 
-//                     data-dismiss="modal"
-//                     onClick={(e) => updateDescription(e)}>
-//                         Edit
-//                     </button>
+                <div className="modal-footer">
+                    <button 
+                    type="button"
+                    className="btn btn-warning" 
+                    data-dismiss="modal"
+                    onClick={(e) => updateDescription(e)}>
+                        Edit
+                    </button>
                     
-//                     <button 
-//                     type="button" 
-//                     className="btn btn-danger" 
-//                     data-dismiss="modal"
-//                     onClick={() => setDescription(todo.description)}>
-//                         Close
-//                     </button>
-//                 </div>
+                    <button 
+                    type="button" 
+                    className="btn btn-danger" 
+                    data-dismiss="modal"
+                    onClick={() => setDescription(todo.description)}>
+                        Close
+                    </button>
+                </div>
 
-//                 </div>
-//             </div>
-//             </div>
-//         </Fragment>
-//     );
-// }
+                </div>
+            </div>
+            </div>
+        </Fragment>
+    );
+}
     
--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,100 +1,44 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
 
-const InputTodo = () => {
-  const [description, setDescription] = useState("");
+export default function InputTodo() {
 
-  const onSubmitForm = async (e) => {
-    e.preventDefault();
-    try {
-      const body = { description };
-      //proxy is only use in development so it will be ignored in production
-      //so if there is no http://localhost:5000 then by default it is going to use heroku domain
-      //remember this heroku app is just our server serving the build static content and also holding the restful api
+    const [description, setDescription] = useState('');
 
-      //https://pern-todo-app-demo.herokuapp.com/todos
-      const response = await fetch("/todos", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-
-      window.location = "/";
-    } catch (err) {
-      console.error(err.message);
+    const onSubmitForm = async e => {
+        e.preventDefault(); 
+        try {
+            const body = { description };
+            const res = await fetch('/todos', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(body) 
+            });
+            console.log(res);
+            window.location = '/';
+        } catch (err) {
+            console.error(err.message);
+        }
     }
-  };
-  return (
-    <Fragment>
-      <h1 className="text-center my-5">Input Todo</h1>
-      <form className="d-flex" onSubmit={onSubmitForm}>
-        <input
-          type="text"
-          placeholder="add todo"
-          className="form-control"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <button className="btn btn-success">Add</button>
-      </form>
-    </Fragment>
-  );
-};
 
-export default InputTodo;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Fragment, useState } from 'react';
-
-// export default function InputTodo() {
-
-//     const [description, setDescription] = useState('');
-
-//     const onSubmitForm = async e => {
-//         e.preventDefault(); 
-//         try {
-//             const body = { description };
-//             const res = await fetch('/todos', {
-//                 method: 'POST',
-//                 headers: { "Content-Type": "application/json"},
-//                 body: JSON.stringify(body) 
-//             });
-//             console.log(res);
-//             window.location = '/';
-//         } catch (err) {
-//             console.error(err.message);
-//         }
-//     }
-
-//     return (
-//         <Fragment>
-//             <h1 className="text-center mt-5">
-//                 PERN Todo List
-//             </h1>
-//             <form 
-//             className="d-flex mt-5"
-//             onSubmit={(e) => onSubmitForm(e)}>
-//                 <input 
-//                 className="form-control"
-//                 type="text" 
-//                 value={description}
-//                 onChange={(e) => setDescription(e.target.value)}
-//                 />
-//                 <button
-//                 className="btn btn-success">
-//                     Add
-//                 </button>
-//             </form>
-//         </Fragment>
-//     );
-// }
+    return (
+        <Fragment>
+            <h1 className="text-center mt-5">
+                PERN Todo List
+            </h1>
+            <form 
+            className="d-flex mt-5"
+            onSubmit={(e) => onSubmitForm(e)}>
+                <input 
+                className="form-control"
+                type="text" 
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                />
+                <button
+                className="btn btn-success">
+                    Add
+                </button>
+            </form>
+        </Fragment>
+    );
+}
