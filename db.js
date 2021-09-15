c5f7fef8970d9910b02aaf023027cbf25ab05f88
Yasin Khan <yasinkhan500@gmail.com>
2021-09-14 19:33:44 -0700

    fixed bug

--- db.js
+++ db.js
@@ -18,20 +18,5 @@ const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devCon
 module.exports = pool;
 
 
-
-
-
-
-// const Pool = require("pg").Pool;
-// require("dotenv").config();
-
-// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
-// const proConfig = process.env.DATABASE_URL; //heroku addons
-
-// const pool = new Pool({
-//   connectionString:
-//     process.env.NODE_ENV === "production" ? proConfig : devConfig,
-// });
-
-// module.exports = pool;
+// ENTER THIS IN TERMINAL!
+// heroku config:set PGSSLMODE=no-verify
